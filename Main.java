0bfeb7663f741d74931c51b5c91fd5e3efd38a32
Luis Fabiano Carvalho Leite <luisfabianodev@gmail.com>
2026-01-16 09:56:06 -0300

    adicionar metodo comecarJogo

---
+++ Main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+
+    Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+    
+    }
+
+    public void comecarJogo() {
+        
+        System.out.println("Começar uma nova batalha? (S/N)");
+        String resp = sc.nextLine().trim();
+
+        while (!resp.equalsIgnoreCase("S") && !resp.equalsIgnoreCase("N")) {
+            System.out.println("Resposta inválida. Digite S ou N:");
+            resp = sc.nextLine().trim();
+        }
+
+        if(resp.equalsIgnoreCase("S")){
+            Arena arena = new Arena();
+            arena.adicionarPersonagens();
+        }else{
+            return;
+        }
+
+    }
+}
