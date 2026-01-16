94fd2d69bd7c663d3aaa6af86965414a08253111
Luis Fabiano Carvalho Leite <luisfabianodev@gmail.com>
2026-01-16 07:37:45 -0300

    Criada a base da classe combatente

---
+++ Combatente.java
@@ -0,0 +1,11 @@
+public class Combatente{
+    private int saude;
+    private String nome;
+    private int experiencia;
+
+    public Combatente(int pv, String nomeDeBatismo, int exp){
+        this.saude = pv;
+        this.nome = nomeDeBatismo;
+        this.experiencia = exp;
+    }
+}
