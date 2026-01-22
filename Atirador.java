75ccf3f96516f8444e3c93ffb1ca311a75a37f2a
Ana Beatriz Soares Dantas <beatriz.dantas@dcomp.ufs.br>
2026-01-22 19:56:58 -0300

    update Atirador.java e Mago.java

--- Atirador.java
+++ Atirador.java
@@ -1,11 +1,9 @@
 public class Atirador extends Combatente{
-    private int pv;
     private int precisao;
     private int ataqueBase;
 
     public Atirador(String nomeDeBatismo, int exp){
-        super( nomeDeBatismo, exp);
-        this.pv = 100;
+        super(nomeDeBatismo, exp, 100 + (exp * 2));
         this.precisao = 10;
         this.ataqueBase = 15;
     }
@@ -18,7 +16,13 @@ public class Atirador extends Combatente{
     }
 
     @Override
-    public void atacar(){
-        
+    public void atacar(Combatente alvo) {
+        int danoTotal = this.ataqueBase + this.experiencia;
+        if (random.nextInt(100) < this.precisao) { 
+            danoTotal *= 2; 
+            System.out.print("[ACERTO CRÍTICO! " + this.precisao + "% de chance] ");
+        }
+        System.out.println(nome + " disparou contra " + alvo.getNome() + " causando " + danoTotal + " de dano.");
+        alvo.receberDano(danoTotal);
     }
 }
--- Mago.java
+++ Mago.java
@@ -1,14 +1,11 @@
 public class Mago extends Combatente{
-    private int pv;
     private int mana;
     private int ataqueBase;
 
     public Mago(String nomeDeBatismo, int exp){
-        super(nomeDeBatismo, exp);
-        this.pv = 150;
+        super(nomeDeBatismo, exp, 150 + (exp * 2));
         this.mana = 10;
-        this.ataqueBase = 15;    
-        
+        this.ataqueBase = 15;
     }
 
     public int getAtaqueBase() {
@@ -20,7 +17,16 @@ public class Mago extends Combatente{
     }
 
     @Override
-    public void atacar(){
-
+    public void atacar(Combatente alvo) {
+        if (mana >= 15) {
+            int danoMagico = (this.ataqueBase * 2) + this.experiencia;
+            mana -= 15;
+            System.out.println(nome + " lança um feitiço em " + alvo.getNome() + "!");
+            alvo.receberDano(danoMagico);
+        } else {
+            System.out.println(nome + " está sem mana! Ataque físico e meditação.");
+            alvo.receberDano(this.ataqueBase);
+            this.mana += 20; 
+        }
     }
 }
